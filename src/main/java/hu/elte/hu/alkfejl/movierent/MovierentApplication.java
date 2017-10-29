edf767da62004a6a30e5a8da2209f2d3e3bd9d20
Daniel Bicsak <bicsak.daniel@gmail.com>
2017-10-29 14:03:47 +0100

    Added genereated Spring Initializer skeleton project items

---
+++ src/main/java/hu/elte/hu/alkfejl/movierent/MovierentApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.hu.alkfejl.movierent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierentApplication.class, args);
+	}
+}
