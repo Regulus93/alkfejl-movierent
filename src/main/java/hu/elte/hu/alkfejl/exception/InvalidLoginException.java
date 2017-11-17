3f1be78a9c03c5976bfdefb8c504dc86ccc4a15d
Daniel Bicsak <bicsak.daniel@gmail.com>
2017-11-18 00:47:34 +0100

    Added exception package

---
+++ src/main/java/hu/elte/hu/alkfejl/exception/InvalidLoginException.java
@@ -0,0 +1,9 @@
+package hu.elte.hu.alkfejl.exception;
+
+public class InvalidLoginException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
